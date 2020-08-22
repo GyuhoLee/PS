ea658504a792328c336e9818514e57c516e5aa77
GyuhoLee <gyuho965@gmail.com>
2020-08-23 08:10:13 +0900

    Add 8718 : Implementation

---
+++ BOJ/08000/8718_Balwanek.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	ll X, K;
+	CIN2(X, K);
+	X *= 1000;
+	K *= 1000;
+	if (K * 7 <= X)	COUT(K * 7);
+	else if (K * 3.5 <= X) COUT(K * 3.5);
+	else if (K * 1.75 <= X) COUT(K * 1.75);
+	else COUT(0);
+
+	return 0;
+}
