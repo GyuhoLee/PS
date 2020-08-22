0545dcea803adbeb4df073adc4099ce828d9656b
GyuhoLee <gyuho965@gmail.com>
2020-08-23 08:12:57 +0900

    Add 8723 : Implementation

---
+++ BOJ/08000/8723_Patyki.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int arr[3];
+	CIN3(arr[0], arr[1], arr[2]);
+	sort(arr, arr + 3);
+	if (arr[0] == arr[2]) COUT(2);
+	else if (arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2]) COUT(1);
+	else COUT(0);
+
+	return 0;
+}
