a74cecdd5fb27e7db4a3967747a3e2db6cf38553
GyuhoLee <gyuho965@gmail.com>
2020-08-23 10:21:16 +0900

    Add 15128 : Implementation

---
+++ BOJ/15000/15128_CongruentNumbers.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	double p1, q1, p2, q2;
+	CIN2(p1, q1);
+	CIN2(p2, q2);
+	double ans = p1 * p2 / q1 / q2 / 2;
+	if (ans == (ll)ans) COUT(1);
+	else COUT(0);
+
+	return 0;
+}
