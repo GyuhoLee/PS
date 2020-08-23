30b92ea40fb0c4822bb9bc34ecaf1a2bea4b3bdc
GyuhoLee <gyuho965@gmail.com>
2020-08-23 09:21:37 +0900

    Add 14264 : Math

---
+++ BOJ/14000/14264_정육각형과삼각형.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	double L;
+	CIN(L);
+	COUT(fixed);
+	cout.precision(10);
+	COUT(L * L * 1.7320508075689 / 4);
+
+	return 0;
+}
