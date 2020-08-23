b7292c99acd1dbd5673f2c52e6241dce1d9d6dc3
GyuhoLee <gyuho965@gmail.com>
2020-08-23 10:37:49 +0900

    Add 15474 : Implementation

---
+++ BOJ/15000/15474_鉛筆.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, A, B, C, D, ans, tmp;
+	CIN3(N, A, B);
+	CIN2(C, D);
+	tmp = N / A;
+	if (N % A) tmp++;
+	ans = tmp * B;
+	tmp = N / C;
+	if (N % C) tmp++;
+	ans = min(ans, tmp * D);
+	COUT(ans);
+
+	return 0;
+}
