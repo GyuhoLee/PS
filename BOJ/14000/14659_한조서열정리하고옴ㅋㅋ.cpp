0c23d7b3c70bb6bba82ef8c99d9033948eb18905
GyuhoLee <gyuho965@gmail.com>
2020-09-18 00:51:01 +0900

    Add 14659 : Brute Force

---
+++ BOJ/14000/14659_한조서열정리하고옴ㅋㅋ.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, arr[30000], ans = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(N);
+	FUP(i, 0, N - 1) CIN(arr[i]);
+	FUP(i, 0, N - 1)
+	{
+		int cnt = 0;
+		FUP(j, i + 1, N - 1)
+		{
+			if (arr[j] > arr[i]) break;
+			else ans = max(ans, ++cnt);
+		}
+	}
+	COUT(ans);
+
+	return 0;
+}
