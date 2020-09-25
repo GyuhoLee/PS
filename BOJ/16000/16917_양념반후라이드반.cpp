97a7e5bdd61c89e3d4eb0272fdc975a189414711
GyuhoLee <gyuho965@gmail.com>
2020-09-25 20:37:06 +0900

    Add 16917 : Math

---
+++ BOJ/16000/16917_양념반후라이드반.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int A, B, C, X, Y, m, ans;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN3(A, B, C);
+	CIN2(X, Y);
+	m = min(X, Y);
+	ans = min(A * (X - m) + B * (Y - m), C * 2 * max(X - m, Y - m));
+	if (A + B > C * 2) ans += m * 2 * C;
+	else ans += A * m + B * m;
+	COUT(ans);
+
+	return 0;
+}
