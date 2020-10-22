df4ada0574a991d1b9c217356698360b4eff75f1
GyuhoLee <gyuho965@gmail.com>
2020-10-22 23:31:21 +0900

    Add 10973 : Permutation

---
+++ BOJ/10000/10973_이전순열.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	CIN(N);
+	vector<int> num(N);
+	FUP(i, 0, N - 1)
+	{
+		CIN(num[i]);
+	}
+	if (!prev_permutation(ALL(num))) COUT(-1);
+	else
+	{
+		for (int n : num)
+		{
+			COUT2(n, "");
+		}
+	}
+
+	return 0;
+}
