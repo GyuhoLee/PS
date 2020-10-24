2a0a6900960a87368ca28dcaa4dd9f2e99272606
GyuhoLee <gyuho965@gmail.com>
2020-10-24 23:59:21 +0900

    Fix Eratosthenes compile error

--- Note/3. Math/Eratosthenes.cpp
+++ Note/3. Math/Eratosthenes.cpp
@@ -12,6 +12,6 @@ void eratosthenes()
         // 이미 체크된 수의 배수들은 합성수임이 자명
         if (arr[i] == 0) continue;
         // i를 제외한 i의 배수들을 0으로 체크
-        for (j = i + i;j <= SIZE;j += i) arr[j] = 0;
+        for (int j = i + i;j <= SIZE;j += i) arr[j] = 0;
     }
 }
