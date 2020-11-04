dd67c277f8249a1cc565c341b315620121d3e7ed
GyuhoLee <gyuho965@gmail.com>
2020-11-04 22:27:04 +0900

    Add 13241 : Euclidean

---
+++ BOJ/13000/13241_최소공배수.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+ll a, b;
+
+ll gcd(ll x, ll y)
+{
+	if (x < y) swap(x, y);
+	ll rest;
+	while (y)
+	{
+		rest = x % y;
+		x = y;
+		y = rest;
+	}
+	return x;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(a, b);
+	ll c = gcd(a, b);
+	COUT(a * (b / c));
+
+	return 0;
+}
