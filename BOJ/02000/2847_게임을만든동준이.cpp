46b4dcfb4159343783a6337a2cc0fd6684acb63d
GyuhoLee <gyuho965@gmail.com>
2020-11-15 19:12:46 +0900

    Add 2847 : Greedy

---
+++ BOJ/02000/2847_게임을만든동준이.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N;
+int arr[101];
+int ans = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(N);
+	FUP(i, 1, N) CIN(arr[i]);
+	FDOWN(i, N - 1, 1)
+	{
+		if (arr[i + 1] <= arr[i])
+		{
+			ans += (arr[i] - arr[i + 1] + 1);
+			arr[i] = arr[i + 1] - 1;
+		}
+	}
+	COUT(ans);
+
+	return 0;
+}
