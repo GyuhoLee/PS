246b59851534e95fe94c01f3d28c6dc79fcc0865
GyuhoLee <gyuho965@gmail.com>
2020-11-22 18:32:43 +0900

    Add 2776 : Hash

---
+++ BOJ/02000/2776_암기왕.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int T, N, M, num;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(T);
+	while (T--)
+	{
+		set<int> S;
+		CIN(N);
+		while (N--)
+		{
+			CIN(num);
+			S.insert(num);
+		}
+		CIN(M);
+		while (M--)
+		{
+			CIN(num);
+			COUT(S.count(num));
+			ENDL;
+		}
+	}
+
+	return 0;
+}
