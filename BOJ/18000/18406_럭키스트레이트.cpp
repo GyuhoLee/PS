50f6171dcec7837fd50dc0cd0a0ab4a4513366e1
GyuhoLee <gyuho965@gmail.com>
2020-11-25 19:14:39 +0900

    Add 18406 : Implementation

---
+++ BOJ/18000/18406_럭키스트레이트.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+string str;
+int N, L = 0, R = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(str);
+	N = str.length();
+	FUP(i, 0, N / 2 - 1) L += (str[i] - '0');
+	FUP(i, N / 2, N - 1) R += (str[i] - '0');
+	if (L == R) COUT("LUCKY");
+	else COUT("READY");
+
+	return 0;
+}
