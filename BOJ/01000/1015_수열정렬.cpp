d2a1abaab69ba6fa46f8270c475c24ad9c80ca37
GyuhoLee <gyuho965@gmail.com>
2020-12-24 21:32:44 +0900

    Add 1015 : Sort

---
+++ BOJ/01000/1015_수열정렬.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, ans[50];
+pair<int, int> P[50];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(N);
+	FUP(i, 0, N - 1)
+	{
+		CIN(P[i].first);
+		P[i].second = i;
+	}
+	sort(P, P + N);
+	FUP(i, 0, N - 1)
+	{
+		ans[P[i].second] = i;
+	}
+	FUP(i, 0, N - 1)
+	{
+		COUT2(ans[i], "");
+	}
+
+	return 0;
+}
