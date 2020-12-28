b685b2037beab2869320aee0816cc9627a3aa5a2
GyuhoLee <gyuho965@gmail.com>
2020-12-28 10:53:57 +0900

    Add 1715 : Priority Queue

---
+++ BOJ/01000/1715_카드정렬하기.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, num, ans = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(N);
+	priority_queue<int, vector<int>, greater<>> pq;
+	FUP(i, 1, N)
+	{
+		CIN(num);
+		pq.push(num);
+	}
+	N--;
+	while (N--)
+	{
+		int a = pq.top();
+		pq.pop();
+		int b = pq.top();
+		pq.pop();
+		ans += a + b;
+		pq.push(a + b);
+	}
+	COUT(ans);
+
+	return 0;
+}
