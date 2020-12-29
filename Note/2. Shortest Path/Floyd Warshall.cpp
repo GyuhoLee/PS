5442047779c4b8983ede4f9e41f22b2e9db772a0
GyuhoLee <gyuho965@gmail.com>
2020-12-29 18:51:47 +0900

    Fix error

--- Note/2. Shortest Path/Floyd Warshall.cpp
+++ Note/2. Shortest Path/Floyd Warshall.cpp
@@ -7,11 +7,11 @@ int main()
 	int d[100][100];
 	FUP(k, 0, N - 1)
 	{
-		FUP(i, 0, N - 1)
+		FUP(j, 0, N - 1)
 		{
-			FUP(j, 0, N - 1)
+			FUP(i, 0, N - 1)
 			{
-				if (d[i][j] > d[j][k] + d[k][j])
+				if (d[i][j] > d[i][k] + d[k][j])
 				{
 					d[i][j] = d[i][k] + d[k][j];
 				}
