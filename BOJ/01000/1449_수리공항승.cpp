3582b6fc0343bc846ecd1c8fdec3edee9f0f1278
GyuhoLee <gyuho965@gmail.com>
2020-12-29 15:26:20 +0900

    Add 1449 : Greedy

---
+++ BOJ/01000/1449_수리공항승.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, L, ans, arr[1000];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, L);
+	FUP(i, 0, N - 1) CIN(arr[i]);
+	sort(arr, arr + N);
+
+	int before = -10000;
+	FUP(i, 0, N - 1)
+	{
+		if (arr[i] - before >= L)
+		{
+			before = arr[i];
+			ans++;
+		}
+	}
+	COUT(ans);
+
+	return 0;
+}
