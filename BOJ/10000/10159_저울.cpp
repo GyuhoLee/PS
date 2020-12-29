cdf5706a51acbe7a2bf2afd681b949fb85c9f5b0
GyuhoLee <gyuho965@gmail.com>
2020-12-29 15:16:40 +0900

    Add 10159 : DFS

---
+++ BOJ/10000/10159_저울.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int a, b, N, M;
+bool visited[101], sv[101], bv[101];
+vector<int> small[101], big[101];
+
+void sDFS(int now)
+{
+	visited[now] = true;
+	sv[now] = true;
+	for (int next : small[now])
+	{
+		if (!sv[next]) sDFS(next);
+	}
+}
+
+void bDFS(int now)
+{
+	visited[now] = true;
+	bv[now] = true;
+	for (int next : big[now])
+	{
+		if (!bv[next]) bDFS(next);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, M);
+	while (M--)
+	{
+		CIN2(a, b);
+		big[a].push_back(b);
+		small[b].push_back(a);
+	}
+	FUP(i, 1, N)
+	{
+		MS(visited, false);
+		MS(sv, false);
+		MS(bv, false);
+		sDFS(i);
+		bDFS(i);
+		int cnt = 0;
+		FUP(j, 1, N)
+		{
+			if (!visited[j]) cnt++;
+		}
+		COUT(cnt);
+		ENDL;
+	}
+
+	return 0;
+}
