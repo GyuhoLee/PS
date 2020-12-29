f111eddc56c50eca516e5631bcca1020a83585ae
GyuhoLee <gyuho965@gmail.com>
2020-12-29 18:13:54 +0900

    Add 1202 : Greedy

---
+++ BOJ/01000/1202_보석도둑.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+ll N, K, ans = 0, pack[300000];
+pair<ll, ll> jewelry[300000];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, K);
+	FUP(i, 0, N - 1) CIN2(jewelry[i].first, jewelry[i].second);
+	FUP(i, 0, K - 1) CIN(pack[i]);
+	sort(jewelry, jewelry + N);
+	sort(pack, pack + K);
+	int idx = 0;
+	priority_queue<ll> pq;
+	FUP(i, 0, K - 1)
+	{
+		while (idx < N && jewelry[idx].first <= pack[i])
+		{
+			pq.push(jewelry[idx++].second);
+		}
+		if (!pq.empty())
+		{
+			ans += pq.top();
+			pq.pop();
+		}
+	}
+	COUT(ans);
+
+
+	return 0;
+}
