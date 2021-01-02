a88c4d6bcc442083a008d6e9e8cc3c25c52b2ada
GyuhoLee <gyuho965@gmail.com>
2021-01-02 20:41:26 +0900

    Add 11003 : Sliding Window

---
+++ BOJ/11000/11003_최솟값찾기.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, L, arr[5000001];
+deque<pair<int, int>> dq;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, L);
+	FUP(i, 1, N)
+	{
+		CIN(arr[i]);
+		if (!dq.empty() && dq.front().second <= i - L) dq.pop_front();
+		while (!dq.empty() && dq.back().first > arr[i])
+		{
+			dq.pop_back();
+		}
+		dq.push_back({ arr[i], i });
+		COUT2(dq.front().first, "");
+	}
+
+	return 0;
+}
