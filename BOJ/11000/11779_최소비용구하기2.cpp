a7c98f17a9cd979afa394f23266d84b7d2eed8e1
GyuhoLee <gyuho965@gmail.com>
2021-01-02 14:55:28 +0900

    Add 11779 : Dijkstra

---
+++ BOJ/11000/11779_최소비용구하기2.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+#define SIZE 1001
+
+int N, M, S, E;	
+int u, v, w;	//u -> v의 가중치 w
+vector<pair<int, int>> arr[SIZE];	//	arr[i] = first까지 second의 가중치
+int d[SIZE]; //시작점에서의 각 idx node 까지의 거리. 처음에 INF값으로 초기화 필요
+int before[SIZE];
+
+void dijkstra()
+{
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({ 0, S });	// S부터 시작
+	while (!pq.empty())
+	{
+		int distance = pq.top().first;
+		int node = pq.top().second;
+		pq.pop();
+		if (distance > d[node]) continue;
+		d[node] = distance;
+		for (auto iter : arr[node])
+		{
+			if (d[iter.first] > d[node] + iter.second)
+			{
+				d[iter.first] = d[node] + iter.second;
+				before[iter.first] = node;
+				pq.push({ d[iter.first], iter.first });
+			}
+		}
+	} // 끝나고도 d[i]가 INF이면 도달할 수 없음.
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	fill(&d[0], &d[SIZE], INT_MAX);
+	CIN2(N, M);
+	while (M--)
+	{
+		CIN3(u, v, w);
+		arr[u].push_back({ v, w });
+	}
+	CIN2(S, E);
+	dijkstra();
+	COUT(d[E]);
+	ENDL;
+	int node = E;
+	stack<int> ans;
+	while (node)
+	{
+		ans.push(node);
+		node = before[node];
+	}
+	COUT(ans.size());
+	ENDL;
+	while (!ans.empty())
+	{
+		COUT2(ans.top(), "");
+		ans.pop();
+	}
+
+	return 0;
+}
