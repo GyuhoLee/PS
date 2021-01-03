f37bedd00faf1ebc01e95e48ef53dc3c7e8e34fc
GyuhoLee <gyuho965@gmail.com>
2021-01-03 21:36:33 +0900

    Add 1940 : Two Pointer

---
+++ BOJ/01000/1940_주몽.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, M, arr[15000], ans = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, M);
+	FUP(i, 0, N - 1) CIN(arr[i]);
+	sort(arr, arr + N);
+	int low = 0, high = N - 1;
+	while (low < high)
+	{
+		if (arr[low] + arr[high] == M)
+		{
+			ans++;
+			low++;
+			high--;
+		}
+		else if (arr[low] + arr[high] < M)
+		{
+			low++;
+		}
+		else
+		{
+			high--;
+		}
+	}
+	COUT(ans);
+
+	return 0;
+}
