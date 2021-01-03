4c7383d0039c08484d99cc1ec4330ece4d589f7c
GyuhoLee <gyuho965@gmail.com>
2021-01-03 18:35:39 +0900

    Add 2841 : Stack

---
+++ BOJ/02000/2841_외계인의기타연주.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, P, ans = 0;
+stack<int> S[300001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, P);
+	while (N--)
+	{
+		int idx, num;
+		CIN2(idx, num);
+		while (!S[idx].empty() && S[idx].top() > num)
+		{
+			S[idx].pop();
+			ans++;
+		}
+		if (S[idx].empty() || S[idx].top() != num)
+		{
+			S[idx].push(num);
+			ans++;
+		}
+	}
+	COUT(ans);
+
+	return 0;
+}
