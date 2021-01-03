81da7c670aad7b28d8565e201b792620b337e193
GyuhoLee <gyuho965@gmail.com>
2021-01-03 21:26:57 +0900

    Add 1269 : Set

---
+++ BOJ/01000/1269_대칭차집합.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int a, b, num, cnt = 0;
+set<int> S;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(a, b);
+	FUP(i, 1, a)
+	{
+		CIN(num);
+		S.insert(num);
+	}
+	FUP(i, 1, b)
+	{
+		CIN(num);
+		if (S.count(num)) cnt++;
+	}
+	COUT(a + b - 2 * cnt);
+
+	return 0;
+}
