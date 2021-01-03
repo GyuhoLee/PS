a108a1b923e6dec5f472fcc9b8587aab617e5d3c
GyuhoLee <gyuho965@gmail.com>
2021-01-03 19:59:15 +0900

    Add 2312 : Math

---
+++ BOJ/02000/2312_수복원하기.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int T, N;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(T);
+	while (T--)
+	{
+		CIN(N);
+		while (N != 1)
+		{
+			FUP(i, 2, N)
+			{
+				int cnt = 0;
+				while (N % i == 0)
+				{
+					N /= i;
+					cnt++;
+				}
+				if (cnt)
+				{
+					COUT2(i, cnt);
+					ENDL;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
