bb0e8f6167cd6ea97e6f8c399b959c380fd40fee
GyuhoLee <gyuho965@gmail.com>
2021-01-14 18:22:50 +0900

    Add 1461 : Greedy

---
+++ BOJ/01000/1461_도서관.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, M, ans = 0;
+vector<int> L, R;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN2(N, M);
+	while (N--)
+	{
+		int num;
+		CIN(num);
+		if (num > 0) R.push_back(num);
+		else L.push_back(num);
+	}
+	sort(ALL(R), greater<int>());
+	sort(ALL(L));
+	for (int i = 0; i < R.size(); i += M) ans += (R[i] * 2);
+	for (int i = 0; i < L.size(); i += M) ans += (L[i] * -2);
+	int last = 0;
+	if (!R.empty()) last = R[0];
+	if (!L.empty()) last = max(last, -L[0]);
+	ans -= last;
+	COUT(ans);
+
+	return 0;
+}
