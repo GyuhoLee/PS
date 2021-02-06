3ec3b2d508793366ab32b2df5f6c709968a93427
GyuhoLee <gyuho965@gmail.com>
2021-02-06 20:35:52 +0900

    Add 17298 : Stack

--- BOJ/17000/17298_오큰수.cpp
+++ BOJ/17000/17298_오큰수.cpp
@@ -1,29 +1,41 @@
-#include<iostream>
-#include<algorithm>
-#include<deque>
-#include<vector>
-#include<stack>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int N, arr[1000001], ans[1000001];
+stack<int> S;
 
 int main()
 {
-	int N;
-	cin >> N;
-	vector<int> V(N), ret(N);
-	stack<int> stk;
-	for (int i = 0; i < N; i++)
-		cin >> V[i];
-	for (int i = N - 1; i >= 0; i--)
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	CIN(N);
+	FUP(i, 1, N) CIN(arr[i]);
+	FDOWN(i, N, 1)
 	{
-		while (!stk.empty() && stk.top() <= V[i])
-			stk.pop();
-		if (stk.empty())
-			ret[i] = -1;
-		else
-			ret[i] = stk.top();
-		stk.push(V[i]);
+		while (!S.empty() && S.top() <= arr[i])
+		{
+			S.pop();
+		}
+		ans[i] = S.empty() ? -1 : S.top();
+		S.push(arr[i]);
 	}
-	for (auto a : ret)
-		cout << a << " ";
+	FUP(i, 1, N) COUT2(ans[i], "");
+
+	return 0;
 }
