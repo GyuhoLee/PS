1a3316448f591b6da9e0e74c6691db3773b76847
GyuhoLee <gyuho965@gmail.com>
2021-02-07 13:14:51 +0900

    Add 2589 : BFS

--- BOJ/02000/2589_보물섬.cpp
+++ BOJ/02000/2589_보물섬.cpp
@@ -1,42 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
 
-int N, M;
+int N, M, ans = 0;
 char arr[50][50];
 bool visited[50][50];
-int dy[4] = { 0, 0, -1, 1 };
-int dx[4] = { 1, -1, 0, 0 };
-int answer = 0;
 
-void input()
-{
-	cin >> N >> M;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++)
-			cin >> arr[i][j];
-}
 
-void solve(int y, int x)
+void BFS(int i, int j)
 {
-	memset(visited, false, sizeof(visited));
 	queue<pair<pair<int, int>, int>> q;
-	q.push({ { y, x }, 0 });
-	visited[y][x] = true;
+	MS(visited, false);
+	q.push({ { i, j }, 0 });
+	visited[i][j] = true;
 	while (!q.empty())
 	{
-		pair<int, int> node = q.front().first;
-		int cnt = q.front().second;
-		answer = max(answer, cnt);
+		int y = q.front().first.first;
+		int x = q.front().first.second;
+		int dist = q.front().second;
 		q.pop();
-		for (int i = 0; i < 4; i++)
+		ans = max(ans, dist);
+		FUP(d, 0, 3)
 		{
-			int ny = node.first + dy[i];
-			int nx = node.second + dx[i];
+			int ny = y + dy[d];
+			int nx = x + dx[d];
 			if (ny < 0 || nx < 0 || ny >= N || nx >= M || visited[ny][nx] || arr[ny][nx] == 'W') continue;
-			q.push({ { ny, nx }, cnt + 1 });
 			visited[ny][nx] = true;
+			q.push({ {ny, nx}, dist + 1 });
 		}
 	}
+
 }
 
 int main()
@@ -44,12 +50,26 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	
-	input();
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++)
-			if(arr[i][j] == 'L') solve(i, j);
-	cout << answer;
+
+	CIN2(N, M);
+	FUP(i, 0, N - 1)
+	{
+		FUP(j, 0, M - 1)
+		{
+			CIN(arr[i][j]);
+		}
+	}
+	FUP(i, 0, N - 1)
+	{
+		FUP(j, 0, M - 1)
+		{
+			if (arr[i][j] == 'L')
+			{
+				BFS(i, j);
+			}
+		}
+	}
+	COUT(ans);
 
 	return 0;
 }
