d51f1d3d75c2ebdb2ade96bbe70000e60831126f
GyuhoLee <gyuho965@gmail.com>
2021-03-27 22:33:05 +0900

    Add 14916 : Math

---
+++ BOJ/14000/14916_거스름돈.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define FUP(i, a, b) for(int i = a; i <= b; i++)
+#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
+#define MS(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end()
+#define CIN(a) cin >> a;
+#define CIN2(a, b) cin >> a >> b
+#define CIN3(a, b, c) cin >> a >> b >> c
+#define COUT(a) cout << a
+#define COUT2(a, b) cout << a << ' ' << b
+#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
+#define ENDL cout << '\n'
+int dy[4] = { -1, 1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	CIN(N);
+	if (N == 1 || N == 3) COUT(-1);
+	else if (N % 2 == 0) COUT(N / 10 * 2 + N % 10 / 2);
+	else
+	{
+		int tmp = (N + 5) % 10;
+		COUT((N - tmp) / 5 + tmp / 2);
+	}
+
+	return 0;
+}
