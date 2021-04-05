9581020ffefb04fb3a4c5c84ac23b1e447c76ac7
GyuhoLee <gyuho965@gmail.com>
2021-04-05 13:42:04 +0900

    Add 5347 : Euclidean

--- BOJ/05000/5347_LCM.cpp
+++ BOJ/05000/5347_LCM.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define FUP(i, a, b) for(int i = a; i <= b; i++)
-#define FDOWN(i, a, b) for(int i = a; i >= b; i--)
-#define MS(a, b) memset(a, b, sizeof(a))
-#define ALL(v) v.begin(), v.end()
-#define CIN(a) cin >> a;
-#define CIN2(a, b) cin >> a >> b
-#define CIN3(a, b, c) cin >> a >> b >> c
-#define COUT(a) cout << a
-#define COUT2(a, b) cout << a << ' ' << b
-#define COUT3(a, b, c) cout << a << ' ' << b << ' ' << c
-#define ENDL cout << '\n'
-int dy[4] = { -1, 1, 0, 0 };
-int dx[4] = { 0, 0, 1, -1 };
+#define ll long long
 
 ll gcd(ll x, ll y)
 {
@@ -30,18 +17,12 @@ ll gcd(ll x, ll y)
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	int T;
-	CIN(T);
-	while (T--)
+	ll N, A, B;
+	cin >> N;
+	while (N--)
 	{
-		ll a, b;
-		CIN2(a, b);
-		COUT(a * b / gcd(a, b));
-		ENDL;
+		cin >> A >> B;
+		cout << A * B / gcd(A, B) << '\n';
 	}
 
 	return 0;
